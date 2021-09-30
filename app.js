e517a2eea23765b0924ab92c3626d1854d60ff47
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-09-30 15:39:49 -0700

    added addition UI and button listener

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const sum = document.getElementById('sum');
+const addButton = document.getElementById('add-button');
+
 
 // initialize global state
 
 // set event listeners 
+
+addButton.addEventListener('click', ()=>{
+  console.log('i am doing the clicking of the button');
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
