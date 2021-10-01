c6787717191493db9c785e6148dd2ef2810dd7af
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-09-30 20:53:54 -0700

    added html and javascript for subtract function

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
+import { add, sub } from './calculations.js';
+
+//addition
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const sum = document.getElementById('sum');
 const addButton = document.getElementById('add-button');
+
+//subtraction
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const difference = document.getElementById('difference');
+const subButton = document.getElementById('sub-button');
+
 
 
 // initialize global state
@@ -10,8 +20,20 @@ const addButton = document.getElementById('add-button');
 // set event listeners 
 
 addButton.addEventListener('click', ()=>{
-  //console.log('i am doing the clicking of the button');
+    const x = Number(addInput1.value); //set x equal to the value of addInput1
+    const y = Number(addInput2.value);
+
+
+    //get the sum of x and y anad store it in a variable
+    const result = add (x, y);
+    sum.textContent = result;
+    
 });
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+subButton.addEventListener('click', ()=>{
+    const x = Number(subInput1.value);
+    const y = Number(subInput2.value);
+
+    const result = sub (x, y);
+    difference.textContent = result;
+});
