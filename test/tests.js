cb9d303c441d015b3451306d743530f8ed396885
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-04 07:45:50 -0700

    tests

--- test/tests.js
+++ test/tests.js
@@ -2,19 +2,19 @@ import { add, sub, multiply, divide } from '../calculations.js';
 
 const test = QUnit.test;
 
-test('add function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = 5;
+// test('add function', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = 5;
 
-    //Act
-    // Call the function you're testing and set the result to a const
-    const actual = add(2, 3);
+//     //Act
+//     // Call the function you're testing and set the result to a const
+//     const actual = add(2, 3);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
 
 test('add function', (expect) =>{
     const add1 = 3;
